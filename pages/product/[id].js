04cb9ad39b4e13d523abc4efaec4915672836be3
Joel Varty <joel@agilitycms.com>
2021-04-20 09:40:38 -0400

    first

---
+++ pages/product/[id].js
@@ -0,0 +1,29 @@
+export async function getServerSideProps({res}) {
+
+	res.setHeader('Cache-Control', 's-maxage=300, stale-while-revalidate')
+
+	return {
+	  props: {
+		data: {
+			dateStr: (new Date()).toISOString()
+		}
+	  },
+	}
+  }
+
+  function Page({ data }) {
+	// Render data...
+
+	return (
+		<section>
+			{ JSON.stringify(data) }
+		</section>
+	)
+
+
+
+  }
+
+
+
+export default Page
